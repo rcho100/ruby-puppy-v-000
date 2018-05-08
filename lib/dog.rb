c6ea920e33e49773b328e667eaa424a5e5d67dcb
Richard Cho <richardcho100@gmail.com>
2018-05-08 01:21:44 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Dog
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+    binding.pry
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
+end
